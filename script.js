76488e93744f95dfd2670cdcdc350f38f5dd53a3
tangwai <tmichael11314@gmail.com>
2023-11-17 13:24:10 -0600

    add HTML content/card divs add JS check if search is empty

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+async function searchLocation() {
+    const button = document.querySelector('.searchButton');
+    button.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const searchTerm = document.querySelector('#search').value;
+
+        const locationNameData = await getData(searchTerm);
+        if (locationNameData) {
+            const location = document.querySelector('.location');
+            location.textContent = `${locationNameData.location.name}, ${locationNameData.location.country}`;
+        } else {
+            return "No search term provided"
+        }
+        console.log('this works');
+    });
+}
+
+async function getData(searchTerm = '') {
+    if (searchTerm.trim() !== '') {
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=3ea9677e497c4c4484a92657231311&q=${searchTerm}&days=3&aqi=yes&alerts=yes`);
+        const data = await response.json();
+        return data;
+    } else {
+        return "No search term provided"
+    }
+}
+
+
 async function getLocationName(searchTerm) {
     const location = document.querySelector('.location');
 
@@ -5,12 +33,6 @@ async function getLocationName(searchTerm) {
     location.textContent = `${locationName.location.name}, ${locationName.location.country}`;
 }
 
-
-async function getData(searchTerm) {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=3ea9677e497c4c4484a92657231311&q=${searchTerm}&days=3&aqi=yes&alerts=yes`)
-    const data = await response.json();
-    return data;
-}
 
 function test() {
     fetch(`http://api.weatherapi.com/v1/forecast.json?key=3ea9677e497c4c4484a92657231311&q=tokyo&days=3&aqi=yes&alerts=yes`)
@@ -22,5 +44,13 @@ function test() {
     })
 }
 
-getLocationName('tokyo');
+async function initial(searchTerm) {
+    const location = document.querySelector('.location');
+
+    const locationName = await getData(searchTerm);
+    location.textContent = `${locationName.location.name}, ${locationName.location.country}`;
+}
+
+searchLocation();
+initial('tokyo');
 test();
